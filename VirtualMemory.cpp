8bd29032fc28d80fdce5b557251d3a313309170f
Guy Harel <guy.harel@mail.huji.ac.il>
2023-06-14 15:46:24 +0300

    initial word on VMread

---
+++ VirtualMemory.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Guy Harel on 6/14/2023.
+//
+
+#include "VirtualMemory.h"
+#include "PhysicalMemory.h"
+
+
+
+
+
+int FindPageToEvict(){
+
+}
+
+
+void VMinitialize() {
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        // TODO - where should I zero? does PMwrite automatically do it for words?
+        PMwrite(i, 0);
+    }
+}
+
+
+int VMread(uint64_t virtualAddress, word_t* value){
+    int bit_index = VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH;
+    int curr_offset = 0;
+    int curr_addy = 0;
+    while (bit_index >= 0){
+        curr_offset = virtualAddress >> bit_index & (2 ^ OFFSET_WIDTH - 1);
+        PMread(curr_addy * PAGE_SIZE + curr_offset, &curr_addy);
+        if (curr_addy == 0){
+            return 0;
+        }
+
+        bit_index -= OFFSET_WIDTH;
+    }
+}
+
+int VMwrite(uint64_t virtualAddress, word_t value){
+
+}
+
